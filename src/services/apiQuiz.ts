0765b4a960634bd93ad3de0ed0685ca584449cea
Guilhermeoliveira10 <144123656+Guilhermeoliveira10@users.noreply.github.com>
2025-03-19 20:30:12 -0300

    Update apiQuiz.ts

--- src/services/apiQuiz.ts
+++ src/services/apiQuiz.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiQuiz = axios.create({
-  baseURL: 'https://api-quiz-6a27.onrender.com', // URL da API de Quizzes
+  baseURL: 'https://api-quiz-6a27.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
